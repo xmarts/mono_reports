17ad69fd423273d85cf7aa9290bd165eabec0d94
gilxmarts <gilberto.santiago@xmarts.com>
2019-07-26 08:28:09 -0500

    Correccon inventarios en transito

--- reportes/models/models.py
+++ reportes/models/models.py
@@ -64,21 +64,22 @@ class ProductTemplate(models.Model):
 			for x6 in row6:
 				self.stock_emer = x6[0]				
 
-class StockPicking(models.Model):
-	_inherit = "stock.picking"
-
-	status = fields.Integer(string="Estatus")
+class PurchaseOrder(models.Model):
+	_inherit = "purchase.order"
 
 	@api.one
-	def action_et(self):
+	def action_stock_et(self):
 		for record in self:
+
 			location_enau = self.env['stock.warehouse'].search([('code', '=', 'ENAU')], limit=1)
 			location_eoc = self.env['stock.warehouse'].search([('code', '=', 'EOC')], limit=1)
 			location_emer = self.env['stock.warehouse'].search([('code', '=', 'EMER')], limit=1)
-			location_id_lot = self.env['stock.location'].search([('name', 'like', 'adjustment')], limit=1)
+
 			fecha_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 			fecha = datetime.now().strftime("%m/%d/%y")
+
 			id_ref = 0
+
 			if record.picking_type_id.default_location_dest_id.id == location_enau.lot_stock_id.id:
 				id_ref = location_enau.lot_stock_id.id
 			elif record.picking_type_id.default_location_dest_id.id == location_eoc.lot_stock_id.id:
@@ -86,25 +87,13 @@ class StockPicking(models.Model):
 			elif record.picking_type_id.default_location_dest_id.id == location_emer.lot_stock_id.id:
 				id_ref = location_emer.lot_stock_id.id
 			else:
-				id_ref = 0		
-			if id_ref != 0:
-				if record.move_ids_without_package:
-					for line in record.move_ids_without_package:
-						nombre = line.product_id.name + ' - ' + fecha
-						cr = self.env.cr
-						sql = "INSERT INTO stock_inventory(name,date,state,company_id,location_id,product_id,filter,exhausted,create_uid,create_date,write_uid,write_date) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-						datas_stock_inventory = (nombre,fecha_hora,'done',line.company_id.id,id_ref,line.product_id.id,'product',True,2,fecha_hora,2,fecha_hora)
-						cr.execute(sql, datas_stock_inventory)
-						
-						max_id = "SELECT max(id) FROM stock_inventory"
-						cr.execute(max_id)
-						
-						idi = cr.fetchone()[0]
-						query = "INSERT INTO stock_inventory_line(inventory_id,product_id,product_uom_id,product_qty,location_id,company_id,theoretical_qty,create_uid,create_date,write_uid,write_date) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-						datas_stock_inventory_line = (idi,line.product_id.id,1,line.product_uom_qty,id_ref,line.company_id.id,0,2,fecha_hora,2,fecha_hora)
-						cr.execute(query, datas_stock_inventory_line)
+				id_ref = 0	
 
-						'''inventario_obj = self.env['stock.inventory']
+			if id_ref != 0:
+				if record.order_line:
+					for line in record.order_line:
+						nombre = line.product_id.name + ' - ' + fecha
+						inventario_obj = self.env['stock.inventory']
 						inventario_val = {
 							'name': nombre,
 							'date': fecha_hora,
@@ -112,32 +101,20 @@ class StockPicking(models.Model):
 							'company_id': line.company_id.id,
 							'location_id': id_ref,
 							'product_id': line.product_id.id,
-							'filter': 'product',
-							'exhausted': True,
-							'create_uid': 0,
-							'create_date': fecha_hora,
-							'write_uid': 0,
-							'write_date': fecha_hora
+							'filter': 'product'
 						}
 						inventario_id = inventario_obj.create(inventario_val)
 						if inventario_id:
 							inventario_linea_obj = self.env['stock.inventory.line']
 							inventario_linea_val = {
-								'inventory_id': inventario_id.id,
 								'product_id': line.product_id.id,
 								'product_uom_id': 1,
 								'product_qty': line.product_uom_qty,
 								'location_id': id_ref,
-								'company_id': line.company_id.id,
-								'theoretical_qty': 0,
-								'create_uid': 0,
-								'create_date': fecha_hora,
-								'write_uid': 0,
-								'write_date': fecha_hora
+								'company_id': line.company_id.id
 							}
-							inventario_linea_id = inventario_linea_obj.create(inventario_linea_val)'''
-
-	@api.multi
-	def button_validate(self):
-		self.action_et()
-		return super(StockPicking, self).button_validate() 					
+							inventario_linea_id = inventario_linea_obj.create(inventario_linea_val)
+	@api.one
+	def button_confirm(self):
+		self.action_stock_et()
+		return super(PurchaseOrder, self).button_confirm()						
